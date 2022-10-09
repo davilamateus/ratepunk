4376591859eaa66ed12765dd76fc4ccdb3088490
Mateus Davilagit config --global user.email davila_mateus@hotmail.com <davila_mateus@hotmail.com>
2022-10-09 17:18:45 +0100

    Verson 2.3 - Fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "@fontsource/noto-sans";
+import "@fontsource/caveat-brush"; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
